0e4e9769e1887a45362e4a3691480b49d3bfea85
Giuseppe <peppe.scopelliti@gmail.com>
2017-04-01 19:09:45 +0100

    Refactoring

--- src/main/java/com/gumtree/parser/LocalParserPerson.java
+++ src/main/java/com/gumtree/parser/LocalParserPerson.java
@@ -12,6 +12,8 @@ import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class LocalParserPerson implements PersonParser {
 
+    private static final String DATE_PATTERN = "dd/MM/yy";
+
     @Override
     public Optional<Person> parsePerson(final String personStr) {
         try {
@@ -20,7 +22,7 @@ public class LocalParserPerson implements PersonParser {
             String name = splitArray[0].trim();
             Gender gender = Gender.valueOf(splitArray[1].trim().toUpperCase());
 
-            LocalDate bDay = parse(splitArray[2].trim(), ofPattern("dd/MM/yy")).minusYears(100);
+            LocalDate bDay = parse(splitArray[2].trim(), ofPattern(DATE_PATTERN)).minusYears(100);
             return Optional.of(new Person(name, gender, bDay));
         } catch (Exception e) {
             return Optional.empty();
